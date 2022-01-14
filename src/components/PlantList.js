b9463ceafa7992111a50244bbc52fd9559cf0ab2
Claudia Scarbrough <rubyharrts@gmail.com>
2022-01-14 10:38:37 -0600

    Plants rendering on page

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
-  state = {
-    plants:[]
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    };
   }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+
+  componentDidMount() {
+    axios.get("http://localhost:3333/plants").then((response) => {
+      this.setState({
+        plants: response.data,
+      });
+    });
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
@@ -16,7 +26,12 @@ export default class PlantList extends Component {
       <main className="plant-list">
         {this.state.plants.map((plant) => (
           <div className="plant-card" key={plant.id} data-testid="plant-card">
-            <img className="plant-image" width="280" src={plant.img} alt={plant.name} />
+            <img
+              className="plant-image"
+              width="280"
+              src={plant.img}
+              alt={plant.name}
+            />
             <div className="plant-details">
               <h2 className="plant-name">{plant.name}</h2>
               <p className="plant-scientific-name">{plant.scientificName}</p>
