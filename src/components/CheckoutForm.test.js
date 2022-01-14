35800aa656c4fcacc550a4058b4fb6d0eea3f6db
Claudia Scarbrough <rubyharrts@gmail.com>
2022-01-14 10:51:30 -0600

    renders without errors test passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,14 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
+import MutationObserver from "mutationobserver-shim";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+  render(<CheckoutForm />);
+  const header = screen.getByText(/checkout form/i);
+  expect(header).toBeInTheDocument();
+});
 
 test("shows success message on submit with form details", () => {});
---
+++ src/hooks/useForm.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const useForm = (initialValues) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(initialValues);
+
+  const handleChanges = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowSuccessMessage(true);
+  };
+  return [values, handleSubmit, handleChanges, showSuccessMessage];
+};
+export default useForm;
